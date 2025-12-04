819576ae2faab22d21f2ed287d2317cca466a0d3
jcgvelez <jcgvelez@indracompany.com>
2025-12-04 13:21:26 -0500

    prueba falla

--- src/main/java/com/ejemplo/ci/App.java
+++ src/main/java/com/ejemplo/ci/App.java
@@ -2,6 +2,6 @@ package com.ejemplo.ci;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo desde Java en macOS!");
+        System.out.println("Hola Mundo desde Java en macOS!);
     }
 }
